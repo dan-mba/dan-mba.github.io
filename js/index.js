bcbc0e2c34e88b628164ac6a309a40928290189a
Dan <35927536+dan-mba@users.noreply.github.com>
2018-02-05 12:48:38 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 $(function() {
-
+  $("#topnav a").on("click", function () {
+    $("#topnav div").removeClass("active");
+    $("div.main div").removeClass("active");
+    $(this).addClass("active");
+    $($(this).data("id")).addClass("active");
+  });
 });

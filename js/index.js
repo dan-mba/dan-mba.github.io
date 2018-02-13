571edb3dcc44433feca8609ec4e6336a493c4395
Dan <35927536+dan-mba@users.noreply.github.com>
2018-02-13 15:30:08 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,5 +22,5 @@ $(function() {
     $(window.location.hash).addClass("active");    
   }
   
- $("#contact a:only-child").css("text-align","center"); 
+ $("#contact a:only-child").parent().css("text-align","center"); 
 });

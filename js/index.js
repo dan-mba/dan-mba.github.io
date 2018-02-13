3ea1a18812f3d5598091f4ef0959290bb797a6ed
Dan <35927536+dan-mba@users.noreply.github.com>
2018-02-13 11:14:33 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,8 +15,7 @@ $(function() {
   if(window.location.hash) {
     $("#topnav div").removeClass("active");
     $("div.main div").removeClass("active");
-    $("nav a[href='" + window.location.hash + "']").addClass("active");
-    console.log("nav a[href='" + window.location.hash + "']");
+    $("nav a[href='" + window.location.hash + "']").parent().addClass("active");
     $(window.location.hash).addClass("active");    
   }
 });

8ec2d925bf4c364b5fbf0e7a174f8ed7e85d0339
Dan <35927536+dan-mba@users.noreply.github.com>
2018-02-14 10:36:13 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,10 +6,10 @@ $(function() {
   $("#topnav a").on("click", function () {
     $("#topnav div").removeClass("active");
     $("div.main div").removeClass("active");
-    $("div.main").hide().show();
+//    $("div.main").hide().show();
     $(this).parent().addClass("active");
     $($(this).data("id")).addClass("active");
-    $("div.main").hide().show(0);
+//    $("div.main").hide().show(0);
 
   });
   
@@ -25,7 +25,7 @@ $(function() {
     $("div.main div").removeClass("active");
     $("nav a[href='" + window.location.hash + "']").parent().addClass("active");
     $(window.location.hash).addClass("active");
-    $("div.main").hide().show(0);
+//    $("div.main").hide().show(0);
   }
   
   $("#contact a:only-child").parent().css("text-align","center"); 

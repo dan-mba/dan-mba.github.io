e2b064aa838717be1d236d438577b1f5de8937f4
Dan <35927536+dan-mba@users.noreply.github.com>
2018-03-05 10:06:58 -0500

    Create angular.js

---
+++ js/angular.js
@@ -0,0 +1,29 @@
+var app = angular.module('ResumeApp', []);
+
+app.controller('MainController',
+['$scope', function($scope) {
+  $scope.apps = [
+    {
+      title: "GPS Fleet Map",
+      url: "",
+      description: "Google map of gps fleet data."
+    }
+  ];
+}]);
+
+app.directive('codeSample', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      info: '='
+    },
+    templateUrl: 'js/codeSample.html',
+    link: function(scope, element, attrs) {
+      scope.showCode = function () {
+        $("#code").removeClass("active");
+        $("#samples").addClass("active");
+        $("#sampback").addClass("active");      
+      };
+    }
+  };
+});

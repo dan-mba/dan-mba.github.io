90cac09ff80b2954bfd7efd3bc5ddc5ccad6687d
Dan <35927536+dan-mba@users.noreply.github.com>
2018-03-06 11:55:19 -0500

    Update angular.js

--- samples/nhtsa/angular.js
+++ samples/nhtsa/angular.js
@@ -1,10 +1,10 @@
 var app = angular.module('nhtsaApp', []);
 var outscope;
 
-app.controller('outputController',['$scope',
+app.controller('outputController',
   function($scope){
     outscope = $scope
     outscope.description = "Before";
     outscope.OverallRating = "X";
-  }]
+  }
 );

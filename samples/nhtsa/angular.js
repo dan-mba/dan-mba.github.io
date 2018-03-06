de334be299a136a197eae4a533e987f8c0337aba
Dan <35927536+dan-mba@users.noreply.github.com>
2018-03-06 10:06:14 -0500

    Update angular.js

--- samples/nhtsa/angular.js
+++ samples/nhtsa/angular.js
@@ -1,14 +1,7 @@
 var app = angular.module('nhtsaApp', []);
 
-app.controller('outputController',[]);
-
-app.directive('vehicleOutput', function () {
-  return {
-    restrict: 'E',
-    scope: {},
-    templateUrl: 'angular.html',
-    link: function(scope, element, attrs) {
-      scope.VehicleDescription = "";
-    })
-  };
-});
+app.controller('outputController',['$scope',
+  function($scope){
+    scope.VehicleDescription = "";
+  }]
+);

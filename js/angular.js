a5ea5878686815825fbab65e2ea600b470cc47f9
Dan <35927536+dan-mba@users.noreply.github.com>
2018-03-28 09:13:10 -0400

    Update angular.js

--- js/angular.js
+++ js/angular.js
@@ -4,10 +4,14 @@ app.controller('MainController',
 ['$scope', function($scope) {
   $scope.apps = [
     {
-      title: "GPS Fleet Map",
+      title: "GPS Static Fleet Map",
       url: "samples/fleetmap/fleetmap.html",
       source: "",
-      description: "Google map of gps fleet data."
+      description: "I developed this application to take data from the GPS provider & display it on a map." +
+                    "This app uses the Google Static Maps API. I used this API because at the time it could be used without a key." +
+                    "Because the maps this API provides are static images, I had to devlop an interface to allow you to zoom & move arround the map." +
+                    "I also needed to determine the geographical center of the coordinates and the maximum latitude & longitude distance from the center," +
+                    "so I could ensure all the items would be displayed on the initial map."
     },
     {
       title: "NHTSA Safety Ratings",

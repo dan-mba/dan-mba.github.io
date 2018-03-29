ffb554ac596acf020dbc9dcaf305d0103601a970
Dan <35927536+dan-mba@users.noreply.github.com>
2018-03-29 09:21:53 -0400

    Update angular.js

--- js/angular.js
+++ js/angular.js
@@ -9,18 +9,18 @@ app.controller('MainController',
       source: "",
       description: "I developed this application to take data from the GPS provider & display it on a map. " +
                   "This app uses the Google Static Maps API. I used this API because at the time it could be used without a key. " +
-                  "Because the maps this API provides are static images, I had to devlop an interface to allow you to zoom " +
-                  "& move arround the map. I also needed to determine the geographical center of the coordinates " +
+                  "Because the maps this API provides are static images, I had to develop an interface to allow you to zoom " +
+                  "& move around the map. I also needed to determine the geographical center of the coordinates " +
                   "and the maximum latitude & longitude distance from the center, " +
                   "so I could ensure all the items would be displayed on the initial map."
     },
     {
-      title: "GPS Javascript Fleet Map",
+      title: "GPS JavaScript Fleet Map",
       url: "samples/fleetmapjs/fleetmap.html",
       source: "",
-      description: "This is an updated version of the Static Fleet Map that uses the Google Maps Javascript API. " +
+      description: "This is an updated version of the Static Fleet Map that uses the Google Maps JavaScript API. " +
                   "I created this after finding out Google updated their Maps API TOS to require a key for all usage. " +
-                  "Using the Javascript API allowed me to create a map with clickable location markers to display the " +
+                  "Using the JavaScript API allowed me to create a map with clickable location markers to display the " +
                   "relevant data right on the map in an info bubble."
       
     },
@@ -29,9 +29,9 @@ app.controller('MainController',
       url: "samples/nhtsa/nhtsa.html",
       source: "",
       description: "I developed this application as I was learning how to access REST APIs. " +
-                  "This application acceseses data from the NHTSA 5 Star Safety Ratings API and " +
+                  "This application accesses data from the NHTSA 5 Star Safety Ratings API and " +
                   "displays the available data for the vehicle model selected. " +
-                  "I recently updatded the app to use jQueryUI stylized select menus for a more modern look " +
+                  "I recently updated the app to use jQueryUI stylized select menus for a more modern look " +
                   "& AngularJS for improved formatting & code readability."
     }
   ];

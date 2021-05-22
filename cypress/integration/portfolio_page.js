a86f56c22e89f4d64d66e4a42df866519f6ce155
Dan <35927536+dan-mba@users.noreply.github.com>
2021-05-22 07:26:16 -0400

    Improve Cypress Tests (#86)
    * add more tests
    
    * change action trigger

--- cypress/integration/portfolio_page.js
+++ cypress/integration/portfolio_page.js
@@ -2,4 +2,20 @@ describe('Portfolio Page', () => {
   it('successfully loads', () => {
     cy.visit('/portfolio')
   })
+
+  it('loads 6 repo cards', () => {
+    // 6 repo cards on inital page
+    cy.get('.MuiCard-root')
+      .should('have.length', 6)
+  })
+
+  it('each repo card should have at least 1 topic', () => {
+    // 6 repo cards on inital page
+    cy.get('.MuiCard-root')
+      .each(($el) => {
+        // each repo should have at least 1 topic
+        cy.wrap($el).find('.MuiChip-root')
+          .should('have.length.gte', 1)
+      })
+  })
 })

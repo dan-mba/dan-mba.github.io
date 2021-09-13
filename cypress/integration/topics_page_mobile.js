c9030a62c190cd65241d1688aa9d8c91c8f49726
Dan <35927536+dan-mba@users.noreply.github.com>
2021-09-13 07:17:06 -0400

    add mobile tests for all pages

---
+++ cypress/integration/topics_page_mobile.js
@@ -0,0 +1,18 @@
+describe('Topics Page', () => {
+  beforeEach(() => {
+    cy.viewport('iphone-7');
+  })
+
+  it('successfully loads', () => {
+    cy.visit('/topics')
+  })
+  
+  it('displays topic chips', () => {
+    cy.get('.MuiChip-root').should('have.length.gte', 1)
+  })
+
+  it('should link to topic page', () => {
+    cy.contains('a', 'javascript').click()
+    cy.url().should('include', '/topics/javascript')
+  })
+})

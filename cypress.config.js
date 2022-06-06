70a8a2c8dcebb6df598841c564f0206ae407d513
Dan <35927536+dan-mba@users.noreply.github.com>
2022-06-06 07:42:25 -0400

    update Cypress config for v10

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:8000",
+    video: false,
+    screenshotOnRunFailure: false,
+    defaultCommandTimeout: 10000,
+    supportFile: false,
+    specPattern: 'cypress/integration/**/*.js',
+    setupNodeEvents(on) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        // `args` is an array of all the arguments that will
+        // be passed to browsers when it launches
+
+        if (browser.isHeadless) {
+          // disable WebGL for headless browsers
+          launchOptions.args.push('--disable-software-rasterizer')
+        }
+
+        // whatever you return here becomes the launchOptions
+        return launchOptions
+      })
+    }
+  }
+})
+

cdb506156383bbe97b194294a3af4fc95d21355c
Dan <35927536+dan-mba@users.noreply.github.com>
2022-07-20 07:13:51 -0400

    rename React component files & pages to jsx

--- src/components/PreloadImage.jsx
+++ src/components/PreloadImage.jsx
@@ -1,8 +1,8 @@
 export default function PreloadImage({image}) {
   return (
     <link rel="preload" as="image"
-      imagesrcset={image.images.sources[0].srcSet}
-      imagesizes={image.images.sources[0].sizes}
+      imageSrcSet={image.images.sources[0].srcSet}
+      imageSizes={image.images.sources[0].sizes}
     />
   )
 }

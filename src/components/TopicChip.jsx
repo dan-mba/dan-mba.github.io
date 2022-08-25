820e548d441ff155ddbdd5f329cde2ef7b384269
Dan <35927536+dan-mba@users.noreply.github.com>
2022-08-25 08:05:32 -0400

    use component prop on TopicChip (#452)

--- src/components/TopicChip.jsx
+++ src/components/TopicChip.jsx
@@ -3,12 +3,11 @@ import {Link} from "@gatsbyjs/reach-router";
 
 export default function TopicChip({name, count, size, ...props}) {
   return (
-    <Link to={`/topics/${name}/`} style={{textDecoration: 'none'}}
+    <Chip color="secondary" variant="outlined" label={name} size={size}
+      component={Link} to={`/topics/${name}/`}
+      avatar={count? <Avatar>{count}</Avatar>: null}
+      style={{cursor: 'pointer', margin: '4px', textDecoration: 'none'}}
       {...props}
-    >
-      <Chip color="secondary" variant="outlined" label={name} size={size}
-        avatar={count? <Avatar>{count}</Avatar>: null} style={{cursor: 'pointer', margin: '4px'}}
-      />
-    </Link>
+    />
   )
 }

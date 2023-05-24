471cc121fd1699efc7e1e074ec10ea11ca0b2ae6
Dan <35927536+dan-mba@users.noreply.github.com>
2023-05-24 08:35:43 -0400

    theming cleanup

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -74,9 +74,11 @@ const lightTheme = createTheme({
 });
 
 export const darkTheme = createTheme({
-  ...lightTheme,
   palette: {
     mode: 'dark',
+    primary: {
+      main: '#212121',
+    },
     secondary: {
       main: lightTheme.util.lightBlue,
     },
@@ -84,15 +86,55 @@ export const darkTheme = createTheme({
       paper: '#121212',
     },
   },
+  breakpoints: {
+    values: {
+      xs: 100,
+      sm: 350,
+      md: 500,
+      lg: 750,
+      xl: 1200,
+    },
+  },
+  spacing: (factor) => `${0.5 * factor}rem`,
   components: {
+    MuiTypography: {
+      styleOverrides: {
+        body1: {
+          maxWidth: '70ch',
+        },
+      }
+    },
+    MuiChip: {
+      variants: [
+        {
+          props: {size: 'large'},
+          style: {fontSize: '1em'},
+        }
+      ]
+    },
     MuiCssBaseline: {
       styleOverrides: {
         body: {
           backgroundColor: '#000',
+          fontSize: '0.875rem',
+          lineHeight: '1.43',
+          letterSpacing: '0.01071em'
         },
+        html: darkScrollbar({track: '#212121', thumb: '#666'})
+        
       },
     },
   },
+  pages: {
+    index: {
+      accentBackground: 'rgb(0, 23, 34)', // darken(theme.palette.secondary.main, 0.7)
+      heroText: 'rgb(140, 217, 255)', 
+      titleURLColor: 'rgb(81, 133, 159)', // lighten(theme.palette.secondary.main, 0.32)
+    },
+  },
+  util: {
+    lightBlue: fullBlue('#004d73')
+  }
 });
 
 export default lightTheme;

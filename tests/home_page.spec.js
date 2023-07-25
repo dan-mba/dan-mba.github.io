1ffff508620b381eae8f8721ffee165db8c61a94
Dan <35927536+dan-mba@users.noreply.github.com>
2023-07-25 08:58:22 -0400

    update nav tests

--- tests/home_page.spec.js
+++ tests/home_page.spec.js
@@ -38,15 +38,7 @@ test.describe('Homepage Desktop Tests',() => {
   test('shows 4 nav elements', async ({ page }) => {
     await page.goto('/');
     const list = page.locator('#desktop-links > *');
-    await expect(list).toHaveCount(5);
-  });
-
-  test('shows portfolio dropdown', async ({ page }) => {
-    await page.goto('/');
-    const portfolio = page.getByRole('button', { name: 'Portfolio' });
-    await portfolio.click();
-    const menu = page.locator('#portfolio-menu a');
-    await expect(menu).toHaveCount(3);
+    await expect(list).toHaveCount(6);
   });
 });
 

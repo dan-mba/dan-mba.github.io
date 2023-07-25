2530473cc1ee64856710e1d747ecdd246a7d7409
Dan <35927536+dan-mba@users.noreply.github.com>
2023-07-25 08:43:41 -0400

    update test for technologies

--- tests/technologies_page.spec.js
+++ tests/technologies_page.spec.js
@@ -2,31 +2,31 @@
 const { test, expect } = require('@playwright/test');
 const AxeBuilder = require('@axe-core/playwright').default;
 
-test.describe('Topics Page Tests', () => {
+test.describe('Technologies Page Tests', () => {
   test('Page loads', async ({ page }) => {
-    await page.goto('/topics/');
+    await page.goto('/technologies/');
   });
 
-  test('displays topic chips', async ({ page }) => {
-    await page.goto('/topics/');
+  test('displays technology chips', async ({ page }) => {
+    await page.goto('/technologies/');
     const chips = page.locator('.MuiChip-root');
     const chipsCount = await chips.count();
     await expect(chipsCount).toBeGreaterThanOrEqual(1);
   });
 
-  test('should link to topic page', async ({ page }) => {
-    await page.goto('/topics/');
+  test('should link to technology page', async ({ page }) => {
+    await page.goto('/technologies/');
     const js = page.getByRole('link', { name: /javascript/i });
     await js.click();
-    await expect(page).toHaveURL(/.*\/topics\/javascript\//)
+    await expect(page).toHaveURL(/.*\/technologies\/javascript\//)
   });
 });
 
-test.describe('Topics - accessibility tests (light)', () => {
+test.describe('Technologies - accessibility tests (light)', () => {
   test.use({ colorScheme: 'light' });
 
   test('should pass axe wcag accessibility tests (light)', async ({ page }) => {
-    await page.goto('/topics/');
+    await page.goto('/technologies/');
     const accessibilityScanResults = await new AxeBuilder({ page })
       .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
       .analyze();
@@ -34,11 +34,11 @@ test.describe('Topics - accessibility tests (light)', () => {
   });
 });
 
-test.describe('Topics - accessibility tests (dark)', () => {
+test.describe('Technologies - accessibility tests (dark)', () => {
   test.use({ colorScheme: 'dark' });
 
   test('should pass axe wcag accessibility tests (dark)', async ({ page }) => {
-    await page.goto('/topics/');
+    await page.goto('/technologies/');
     const accessibilityScanResults = await new AxeBuilder({ page })
       .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
       .analyze();

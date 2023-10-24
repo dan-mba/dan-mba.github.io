94905931ab8f59083f6f8b85b949831740f61f05
Dan <35927536+dan-mba@users.noreply.github.com>
2023-10-24 08:30:27 -0400

    add WCAG 2.2 (#730)

--- tests/projects_page.spec.js
+++ tests/projects_page.spec.js
@@ -31,7 +31,7 @@ test.describe('Projects - accessibility tests (light)', () => {
   test('should pass axe wcag accessibility tests (light)', async ({ page }) => {
     await page.goto('/projects/');
     const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22aa"])
       .analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
   });
@@ -43,7 +43,7 @@ test.describe("Projects - accessibility tests (dark)", () => {
   test('should pass axe wcag accessibility tests (dark)', async ({ page }) => {
     await page.goto('/projects/');
     const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22aa"])
       .analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
   });
